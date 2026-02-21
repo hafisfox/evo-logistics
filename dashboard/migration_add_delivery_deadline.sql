395b7b2dc2826328772cf4163fbde075623cd9a1
Hafis Javad <hafisjavad@gmail.com>
2026-02-21 11:31:04 +0700

    chore: remove legacy migration files and testing scripts

--- dashboard/migration_add_delivery_deadline.sql
+++
@@ -1,8 +0,0 @@
--- Add missing delivery_deadline column to master_rfqs
-ALTER TABLE master_rfqs
-ADD COLUMN IF NOT EXISTS delivery_deadline DATE;
-
--- Add missing statuses to the rfq_status enum
-ALTER TYPE rfq_status ADD VALUE IF NOT EXISTS 'Reminded';
-ALTER TYPE rfq_status ADD VALUE IF NOT EXISTS 'Followed_Up';
-ALTER TYPE rfq_status ADD VALUE IF NOT EXISTS 'Customer_Replied';
